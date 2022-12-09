9bb700a57900d15fa2c9ab6f79f33812ee04a08e
dawMP - Sánchez Blanco <A22FatimaSB@sanclemente.local>
2022-12-09 18:28:13 +0100

    ejercicios

---
+++ Exercicio repaso formulario PHP/php/insertar_csv.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <h1>Resultado del envío de la información de los formularios/h1>
+
+        <p>
+            <?php
+            print_r($_POST)
+            ?>
+            </p>
+    </body>
+</html>
